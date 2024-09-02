5fd5a4b1c3106cdf5482ab55d9c3a16e24c75f57
Harmony <harmonymukolwe@gmail.com>
2024-09-02 23:40:05 +0300

    Creates anagram detector

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,10 @@
 # your code goes here!
+
+class Anagram:
+  def __init__(self,word):
+    self.word = sorted(word.lower())
+
+  def match(self,list_of_words):
+    sorted_list_of_words = [sorted(possible_anagram.lower()) for possible_anagram in list_of_words]
+    list_of_anagrams = [anagram for anagram in list_of_words if sorted_list_of_words[list_of_words.index(anagram)] == self.word]
+    return list_of_anagrams
